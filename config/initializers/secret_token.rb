7a2610153af57885720762b52b1c0a0fa1868e84
Chris Nicola <chnicola@gmail.com>
2013-11-14 21:27:08 -0800

    Initial checking of Hackerleader

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackerleader::Application.config.secret_key_base = '79ebbf6fe0a3ac138d2e80aeb79e95626637253f64421ffa98d3b82948e83d140b2de9f6f3df79fdf4fd8fc71eb5cbf4ce6e618ebc5620c6d4abe4f6642e705f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hackerleader::Application.config.session_store :cookie_store, key: '_hackerleader_session'
